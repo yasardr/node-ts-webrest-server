338bfb8c28650d547c9ca3353ec2b0e69208d5b3
Yasar Altamirano <droidhp.ya@gmail.com>
2024-10-03 18:08:47 -0600

    Fin seccion 16

---
+++ src/domain/datasources/todo.datasource.ts
@@ -0,0 +1,13 @@
+import { TodoEntity } from '../entities/todo.entity';
+import { CreateTodoDto, UpdateTodoDto } from '../dtos';
+
+export abstract class TodoDatasource {
+
+    abstract create(createTodoDto: CreateTodoDto): Promise<TodoEntity>;
+    //todo: paginación
+    abstract getAll(): Promise<TodoEntity[]>;
+    abstract findById(id: number): Promise<TodoEntity>;
+    abstract updateById(updateTodoDto: UpdateTodoDto): Promise<TodoEntity>;
+    abstract deleteById(id: number): Promise<TodoEntity>;
+
+}
